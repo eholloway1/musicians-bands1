cd794656719b05bf5e10bf49ada426f7dbe6204c
Evan Holloway <eholloway00@gmail.com>
2024-02-15 17:26:40 -0800

    complete band/music/song framework, finally understand sequelize test...maybe

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ const { Musician } = require('./models/Musician')
 const { Song } = require("./models/Song")
 // Define associations here
 
+Band.hasMany(Musician);
+Musician.belongsTo(Band);
 
+Band.belongsToMany(Song, {through: "BandSong"});
+Song.belongsToMany(Band, {through: "BandSong"});
 
 module.exports = {
     Band,
